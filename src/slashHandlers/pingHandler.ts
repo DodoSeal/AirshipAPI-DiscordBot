406b0c4f9cee6debe738f9c089f2f45e1f801589
DodoSeal <maxhmcd08@gmail.com>
2026-07-22 22:10:44 -0600

    Feat: Ping command using dynamic interaction handler

---
+++ src/slashHandlers/pingHandler.ts
@@ -0,0 +1,11 @@
+import { ChatInputCommandInteraction, Client } from "discord.js";
+import { SlashCommandHandler } from "../types/AppEventTypes.js";
+
+const command: SlashCommandHandler = {
+    name: "ping",
+    execute: async function(app: Client, interaction: ChatInputCommandInteraction) {
+        await interaction.reply("Pong!");
+    }
+};
+
+export default command;
